355d993be8f2eee6b9ef87c7c045558730217b28
= <webdev.jameslemire@gmail.com>
2017-04-19 11:16:39 -0600

    JL solution - scratch everything.

---
+++ app/src/component/Navigation/Navigation.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Navigation.css';
+
+class Navigation extends Component {
+  render() {
+    return (
+      <div id="navigation-component">
+        <p id="navigation-header"> Topics </p>
+      </div>
+    )
+  }
+}
+
+export default Navigation;
