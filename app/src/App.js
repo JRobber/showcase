60558206353c1bdb4315f93bc46c781e4f70f1fd
= <webdev.jameslemire@gmail.com>
2017-04-19 11:16:26 -0600

    JL solution - scratch everything.

--- app/src/App.js
+++ app/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 // Components
 import Header from './component/Header/Header';
+import Navigation from './component/Navigation/Navigation';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <Navigation />
       </div>
     )
   }
